a0c8c8627e52eea33bff6551bc5b50d19533c3b5
VisH317 <vt201916384@gmail.com>
2024-05-23 20:58:38 -0400

    updating data

---
+++ data/split.py
@@ -0,0 +1,27 @@
+import torch
+import pickle
+from torch import Tensor
+from tqdm import tqdm
+
+SEQLEN = 1024
+
+def process(data: str):
+    print("loading data...")
+    with open(data, "rb") as f:
+        msas = pickle.load(f)
+
+    proc = []
+
+    for ix, msa in tqdm(enumerate(msas), desc="parsing"):
+        x, target = msa
+        proc.append(tuple([x[:, :512], target[:, :512]]))
+        proc.append(tuple([x[:, 512:], target[:, 512:]]))
+    
+    print("Storing data...")
+    with open("msa_seq1k_split_30k.pkl", "wb") as f:
+        pickle.dump(proc, f)
+    
+
+
+if __name__ == "__main__":
+    process("msa_seq1k_30k.pkl")
